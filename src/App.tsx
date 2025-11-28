dd9621e387c850afb3a03e7be8bcabd2307235fd
Muhammad Aniq Mir <aniqmir@gmail.com>
2025-11-28 16:19:40 +0500

    main file update

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import { useState } from 'react';
-import { Toaster } from 'react-hot-toast';
+import { Routes, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar';
+import Home from "./pages/Home";
+import ToddlerActivityGenerator from "./pages/Tools/ToddlerActivityGenerator";
 
-import NotesApp from './miniApps/NotesApp';
-import StopwatchApp from './miniApps/StopwatchApp';
-import ImageGalleryApp from './miniApps/ImageGalleryApp';
 
-function App() {
-  const [miniApp, setMiniApp] = useState<number>(0);
-
-  const handleMiniAppChange = (miniAppId: number) => {
-    setMiniApp(miniAppId);
-  };
-
-  const MiniAppsBackgroundURL =
-    'https://images.pexels.com/photos/5011647/pexels-photo-5011647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-
+export default function App() {
   return (
-    <>
-      <Toaster />
-      <Navbar handleMiniAppChange={handleMiniAppChange} />
-      {miniApp === 0 && (
-        <div
-          style={{
-            backgroundImage: `url(${MiniAppsBackgroundURL})`,
-          }}
-          className='bg-center bg-cover bg-no-repeat h-screen'
-        />
-      )}
-      {miniApp === 1 && <NotesApp />}
-      {miniApp === 2 && <StopwatchApp />}
-      {miniApp === 3 && <ImageGalleryApp />}
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<Home />} />
+      <Route path="/toddler-activity-generator" element={<ToddlerActivityGenerator />} />
+      {/* <Route path="/about" element={<About />} /> */}
+    </Routes>
   );
 }
-
-export default App;
-
